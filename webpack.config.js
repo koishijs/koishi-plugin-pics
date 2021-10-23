01ad725762642483c99beacdf3901d1898772b6a
purerosefallen <78877@qq.com>
2021-10-23 17:26:12 +0800

    use schemagen as external

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.ts",
-  mode: "production",
-  target: "node",
-  devtool: "source-map",
+  entry: './src/index.ts',
+  mode: 'production',
+  target: 'node',
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
-      { test: /\.mustache$/, use: "raw-loader" },
+      { test: /\.mustache$/, use: 'raw-loader' },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "index.js",
+    filename: 'index.js',
     library: {
-      type: "commonjs",
+      type: 'commonjs',
     },
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
   externals: {
-    'koishi': 'koishi',
-  }
+    koishi: 'koishi',
+    'koishi-utils-schemagen': 'koishi-utils-schemagen',
+  },
 };
