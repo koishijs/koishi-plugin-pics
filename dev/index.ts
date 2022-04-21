9d1f2c22c3e79f1fd6e4d5d7a442419cb65988ab
purerosefallen <78877@qq.com>
2022-04-21 22:50:39 +0800

    bump things

--- dev/index.ts
+++ dev/index.ts
@@ -2,7 +2,7 @@ import { App } from 'koishi';
 import TargetPlugin from '../src';
 import ConsolePlugin from '@koishijs/plugin-console';
 import SandboxPlugin from '@koishijs/plugin-sandbox';
-import DatabasePlugin from '@koishijs/plugin-database-memory';
+import * as DatabasePlugin from '@koishijs/plugin-database-memory';
 import CachePlugin from '@koishijs/plugin-cache-lru';
 import ExtrasInDev from './extras';
 
